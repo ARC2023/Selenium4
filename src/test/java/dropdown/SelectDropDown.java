2f31c42aa76206d13c4e5c4d35f616a8ca18c72d
ARC2023 <arcautomation2023@gmail.com>
2023-08-17 09:30:31 +0530

    Dropdown

---
+++ src/test/java/dropdown/SelectDropDown.java
@@ -0,0 +1,52 @@
+package dropdown;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class SelectDropDown {
+
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		
+		
+		driver.get("https://www.facebook.com/");
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//a[text()='Create new account']")).click();
+		Thread.sleep(3000);
+		
+		WebElement day= driver.findElement(By.id("day"));
+		WebElement month= driver.findElement(By.id("month"));
+		WebElement year= driver.findElement(By.id("year"));
+		
+		
+		Select daySelect= new Select(day);		
+		daySelect.selectByIndex(8);
+		Thread.sleep(3000);
+		daySelect.selectByValue("29");
+		Thread.sleep(3000);		
+		daySelect.selectByVisibleText("15");
+		
+		
+		Select monthSelect= new Select(month);		
+		monthSelect.selectByIndex(8);
+		Thread.sleep(3000);
+		monthSelect.selectByValue("3");
+		Thread.sleep(3000);		
+		monthSelect.selectByVisibleText("Feb");
+		
+		
+		Select yearSelect= new Select(year);		
+		yearSelect.selectByIndex(23);
+		Thread.sleep(3000);
+		yearSelect.selectByValue("1993");
+		Thread.sleep(3000);		
+		yearSelect.selectByVisibleText("1991");
+		
+		
+		
+	}
+
+}
